64023b20c8f289b28ebb71ff466559c003ca0aad
Arthur Piloto <arthurpiloto.github@gmail.com>
2025-05-21 13:17:13 -0300

    FEAT - exercício 1.2 lista 2 finalizado

--- deque_numeros_negativos/deque.h
+++ deque_numeros_negativos/deque.h
@@ -15,7 +15,7 @@
 
 typedef struct NO {
     int info;
-    NO *prox, *ant;
+    struct NO *prox, *ant;
 } NO;
 
 NO* alocarNO() {
@@ -40,6 +40,18 @@ Deque* criaDeque() {
     return dq;
 }
 
+void destroiDeque(Deque *dq) {
+    if (dq != NULL) {
+        NO *aux;
+        while (dq->ini != NULL) {
+            aux = dq->ini;
+            dq->ini = dq->ini->prox;
+            liberarNO(aux);
+        }
+        free(dq);
+    }
+}
+
 int estaVazio(Deque *dq) {
     return (dq->ini == NULL);
 }
@@ -62,6 +74,19 @@ int insereFim(Deque *dq, int elem) {
     return 1;
 }
 
+int removeFim(Deque *dq) {
+    if (dq == NULL || estaVazio(dq)) return 0;
+    NO *aux = dq->fim;
+    if (dq->ini == dq->fim) dq->ini = dq->fim = NULL;
+    else {
+        dq->fim = dq->fim->ant;
+        dq->fim->prox = NULL;
+    }
+    liberarNO(aux);
+    dq->qtd--;
+    return 1;
+}
+
 int insereIni(Deque *dq, int elem) {
     if (dq == NULL) return 0;
     NO *novo = alocarNO();
@@ -69,15 +94,60 @@ int insereIni(Deque *dq, int elem) {
     novo->info = elem;
     novo->prox = NULL;
     if (estaVazio(dq)) {
-        novo->ant = NULL;
-        dq->ini = novo;
+        novo->prox = NULL;
+        dq->fim = novo;
     } else {
-        dq->fim->prox = novo;
-        novo->ant = dq->fim;
+        dq->ini->ant = novo;
+        novo->prox = dq->ini;
     }
-    dq->fim = novo;
+    dq->ini = novo;
     dq->qtd++;
     return 1;
+}
+
+int removeIni(Deque *dq) {
+    if (dq == NULL || estaVazio(dq)) return 0;
+    NO *aux = dq->ini;
+    if (dq->ini == dq->fim) dq->ini = dq->fim = NULL;
+    else {
+        dq->ini = dq->ini->prox;
+        dq->ini->ant = NULL;
+    }
+    liberarNO(aux);
+    dq->qtd--;
+    return 1;
+}
+
+void imprime(Deque *dq) {
+    if (dq == NULL || estaVazio(dq)) return;
+    NO *aux = dq->ini;
+    printf("Elementos: ");
+    while(aux != NULL) {
+        printf("%d ", aux->info);
+        aux = aux->prox;
+    }
+    printf("\n");
+}
+
+int removeNegativos(Deque *dq) {
+    if (dq == NULL || estaVazio(dq)) return 0;
+    NO *aux = dq->ini;
+    while (aux != NULL) {
+        NO *prox = aux->prox;
+
+        if (aux->info < 0) {
+            if (aux->ant != NULL) aux->ant->prox = aux->prox;
+            else dq->ini = aux->prox;
+
+            if (aux->prox != NULL) aux->prox->ant = aux->ant;
+            else dq->fim = aux->ant;
+
+            liberarNO(aux);
+            dq->qtd--;
+        }
+
+        aux = prox;
+    }
 }
 
 #endif
--- deque_numeros_negativos/main.c
+++ deque_numeros_negativos/main.c
@@ -2,21 +2,17 @@
 #include "deque.h"
 
 int main() {
-    Deque *palin = criaDeque();
-    insereFim(palin, 'o');
-    insereFim(palin, 'v');
-    insereFim(palin, 'o');
-    Deque *naoPalin = criaDeque();
-    insereFim(naoPalin, 'e');
-    insereFim(naoPalin, 'v');
-    insereFim(naoPalin, 'a');
-    imprime(palin);
-    if (verificaPalindromo(palin)) printf("É palindromo!\n");
-    else printf("Não é palindromo!\n");
-    imprime(naoPalin);
-    if (verificaPalindromo(naoPalin)) printf("É palindromo!\n");
-    else printf("Não é palindromo!\n");
-    destroiDeque(palin);
-    destroiDeque(naoPalin);
+    Deque *dq = criaDeque();
+    insereFim(dq, 1);
+    insereIni(dq, -1);
+    insereFim(dq, 2);
+    insereFim(dq, -9);
+    insereFim(dq, 3);
+    insereIni(dq, -15);
+    insereFim(dq, 4);
+    imprime(dq);
+    printf("Removendo negativos...\n");removeNegativos(dq);
+    imprime(dq);
+    destroiDeque(dq);
     return 0;
 }
